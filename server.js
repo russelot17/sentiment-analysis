0b14f33c80a550fe25973f99d92145be7c010c2b
russelot17 <russelucero@gmail.com>
2021-05-14 22:04:05 +0800

    cors test

--- server.js
+++ server.js
@@ -2,7 +2,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static('./dist/sentiment-analyzer'));
+
+
+// app.use(express.static('./dist/sentiment-analyzer'));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/sentiment-analyzer/'}),
