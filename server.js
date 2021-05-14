94114c85ea105d6945b0674b0236340c1d540a87
russelot17 <russelucero@gmail.com>
2021-05-14 12:32:44 +0800

    cors issue

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const path = require('path');
 const app = express();
 app.use(express.static('./dist/sentiment-analyzer'));
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/sentiment-analyzer/'}),
 );
